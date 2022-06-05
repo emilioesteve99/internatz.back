856b8ddfe373188c2df6160a172bce60fe7ab347
Emilio <emespei@icloud.com>
2022-06-05 10:01:00 +0200

    feat: si hay enterprise id en el body de la request y no coincide con el enterpriseId del usuario, tiro un error de unauthorized

--- src/modules/_shared/guard/EndpointPermission.guard.ts
+++ src/modules/_shared/guard/EndpointPermission.guard.ts
@@ -6,12 +6,14 @@ import { Request } from "express";
 @Injectable()
 export class EndpointPermissionGuard implements CanActivate {
 	canActivate(context: ExecutionContext): boolean {
+		const { user } = RequestContext.get();
 		const httpContext = context.switchToHttp();
 		const req: Request = httpContext.getRequest();
+		const exception = new UnauthorizedUserException();
+		const enterpriseId = req.body?.enterpriseId;
+		if (enterpriseId && enterpriseId !== user.enterpriseId) throw exception;
 		const url = new URL(`http://localhost:4100${req.originalUrl}`).pathname;
 		const endpoint = url.toLowerCase().replace('/api/internatz-back', '');
-		const exception = new UnauthorizedUserException();
-		const { user } = RequestContext.get();
 		if (user.isAdmin) return true;
 		if (!user) throw exception;
 		if (!user.hasPermission('endpoint', endpoint)) throw exception;
